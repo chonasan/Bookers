3cb0b7cefdbb00395cda62c4c3ae78da888232e9
chonasan <tk24120824@gmail.com>
2021-01-28 06:00:06 +0000

    Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,26 @@
 class BooksController < ApplicationController
+  def new
+  end
+  
   def index
+    @book = Book.new
   end
 
   def show
   end
-
-  def new
-    @blog = Book.new
-  end
   
   def create
+    book = Book.new(blog_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
   
-  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
   
 end
