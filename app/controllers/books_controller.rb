34a7589621b535c03ff4e5c51916d79620a18a98
chonasan <tk24120824@gmail.com>
2021-02-01 07:32:26 +0000

    Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,13 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+   @book = Book.find(params[:id])
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(blog.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
