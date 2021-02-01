47ea4ff1b2a1438123e55929c063b9c167cfd20b
chonasan <tk24120824@gmail.com>
2021-02-01 09:10:19 +0000

    Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,13 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+   if @book.save
     redirect_to book_path(book.id)
+   else
+    render action: :index
+   end
   end
 
   def edit
